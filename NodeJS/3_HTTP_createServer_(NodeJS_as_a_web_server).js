94f61cfc3b1ee7c79d467871c52888f09cba2b69
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2021-10-01 01:17:59 +0530

    Link for the documentation's page for server creation is added.

--- NodeJS/3_HTTP_createServer_(NodeJS_as_a_web_server).js
+++ NodeJS/3_HTTP_createServer_(NodeJS_as_a_web_server).js
@@ -1,6 +1,8 @@
 // NodeJs as as Web Server
 // Visit the documentation of NodeJS for Guides and syntax documentation
 // A better version of this example is present in official documentation of NodeJS
+// visit https://nodejs.org/en/docs/guides/getting-started-guide/
+
 
 console.log(`The HTTP module can create an HTTP server that listens to server ports and gives a response back to the client.`)
 
