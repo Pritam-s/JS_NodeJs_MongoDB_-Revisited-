c16ecfca126d6bdf04fe94f7cb764f33557bde7a
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2021-09-30 22:23:37 +0530

    Initial commit --> 2_NodeJS - HTTP Module Introduction.

---
+++ NodeJS/2_HTTP_Module.js
@@ -0,0 +1,7 @@
+// The Built-in HTTP module
+
+console.log(`Node.js has a built-in module called HTTP, which allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP)`)
+
+// To include the HTTP module, use the require() method.
+
+const http = require('http');
