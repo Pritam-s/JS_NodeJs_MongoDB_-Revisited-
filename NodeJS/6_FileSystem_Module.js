6dc08a4e4dc9e08705839ff87c5945bc7c65be38
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2021-10-01 23:02:28 +0530

    Initial commit. Fs Intro.

---
+++ NodeJS/6_FileSystem_Module.js
@@ -0,0 +1,12 @@
+// The Node.js file system allows you to work with the file system on your computer.
+// To include the file system module, use the require() method.
+
+var fs = require('fs');
+
+/* common use for the file system module.
+1. Read files
+2. Create files
+3. Update files
+4. Delete files
+5. Rename files
+*/
